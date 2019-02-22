5ee5a5422e152fde1769bb20032faf1d8f51f37a
NKragh <39466838+NKragh@users.noreply.github.com>
2019-02-22 09:45:19 +0100

    1337 H4X
    sinds

---
+++ DelegateBankExample/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateBankExample
+{
+    class Account
+    {
+        private int _balance;
+        private bool _medarbejder;
+
+        public delegate int Deposit(int deposit);
+        public delegate int Withdrawal(int withdrawal);
+
+        public Deposit AccountDeposit;
+        public Withdrawal AccountWithdrawal;
+
+        public Account(bool medarbejder)
+        {
+            _medarbejder = medarbejder;
+        }
+        public int Add(int deposit)
+        {
+            if (_medarbejder == false && deposit > 50000)
+            {
+                Console.WriteLine("Du sætter meget ind mester. Tror du ikke du skulle ringe til SKAT?");
+            }
+            else if (_medarbejder == true && deposit > 100000)
+            {
+                Console.WriteLine("Du sætter vist lidt meget ind mesterløgsovs.");
+            }
+            else
+            {
+                _balance += deposit;
+
+            }
+            return Balance;
+        }
+
+        public int Balance
+        {
+            get { return _balance; }
+        }
+
+        public bool Medarbejder
+        {
+            get { return _medarbejder; }
+            set { _medarbejder = value; }
+        }
+    }
+}
--- DelegateBankExample/Program.cs
+++ DelegateBankExample/Program.cs
@@ -10,6 +10,19 @@ namespace DelegateBankExample
     {
         static void Main(string[] args)
         {
+            Account NikolajsAccount = new Account(true);
+
+            NikolajsAccount.AccountDeposit = NikolajsAccount.Add;
+
+            Console.WriteLine(NikolajsAccount.AccountDeposit(3));
+            Console.WriteLine(NikolajsAccount.AccountDeposit(99000));
+
+            Account JohanAccount = new Account(false);
+
+            JohanAccount.AccountDeposit = JohanAccount.Add;
+
+            Console.WriteLine(JohanAccount.AccountDeposit(49999));
+            Console.WriteLine(JohanAccount.AccountDeposit(50001));
         }
     }
 }
